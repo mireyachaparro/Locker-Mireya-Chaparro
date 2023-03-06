b6a5f453e2ea0174e5cb434475fcf9c482412a8a
mireyachaparro <mireyacn99@gmail.com>
2023-03-06 12:59:59 +0100

    add the connection to the user database

---
+++ public_html/conn.php
@@ -0,0 +1,6 @@
+<?php
+$dbhost	= process.env.dbhost;	   		 // localhost or IP
+$dbuser	= process.env.dbuser;		  		 // database username
+$dbpass	= process.env.dbpass1;		     // database password
+$dbname	= process.env.dbname1;    		// database name
+?>
