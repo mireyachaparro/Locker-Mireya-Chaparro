c2e416883de8204d30a2d379c8186f73be32d38e
mireyachaparro <mireyacn99@gmail.com>
2023-03-06 12:20:40 +0100

    Add index page

---
+++ public_html/index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>Inicio</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!--<link rel="icon" href="images/logo.jpg" type="image/jpg"/> -->
+        <link rel="shortcut icon" href="images/logo.ico" type="image/x-icon"/>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+		<link rel="stylesheet" href="custom.css">
+		<script src="https://kit.fontawesome.com/6e0e39de35.js" crossorigin="anonymous"></script>
+  </head>
+  <body>
+	<?php
+	include "header.php";
+	?>
+<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<h1> </h1>
+		</div>
+	</div>
+		<div class="col-sm-12 col-md-6 col-lg-6">
+			<h3>Bienvenido</h3><hr/>
+			<table><tr><th width="50%"><a id="tres" class="btn btn-link" href="listarpuertas.php"><i class="fas fa-sign-in-alt fa-10x" aria-hidden="true"></i></a>
+			</th><th width="50%"></th><th width="50%">
+			<a id="tres" class="btn btn-link" href="listarpuertas2.php"><i class="fas fa-sign-out-alt fa-10x" aria-hidden="true"></i></a>
+			</th></tr><tr><td>Guardar</td><td></td><td>Recoger</td></tr></table>
+		</div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+ 
+	</body>
+</html>
