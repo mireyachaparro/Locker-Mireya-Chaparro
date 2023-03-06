a296afa4d6eef4d44bae04e5d5cbd341a6d05bcd
mireyachaparro <mireyacn99@gmail.com>
2023-03-06 12:59:45 +0100

    add the connection with the locker database

---
+++ public_html/conexion2.php
@@ -0,0 +1,6 @@
+<?php
+$dbhost	= process.env.dbhost;	   		 // localhost or IP
+$dbuser	= process.env.dbuser;		  		 // database username
+$dbpass	= process.env.dbpass2;		     // database password
+$dbname	= process.env.dbname2;    		// database name
+?>
