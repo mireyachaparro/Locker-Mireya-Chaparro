ad4dd52326130bf182fe72fe62dea76724264eb7
mireyachaparro <mireyacn99@gmail.com>
2023-03-06 12:10:25 +0100

    Add contact page

---
+++ public_html/contacto.php
@@ -0,0 +1,46 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>Contacto</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!--<link rel="icon" href="images/logo.jpg" type="image/jpg"/> -->
+        <link rel="shortcut icon" href="images/logo.ico" type="image/x-icon"/>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+		<link rel="stylesheet" href="custom.css">
+		<script src="https://kit.fontawesome.com/6e0e39de35.js" crossorigin="anonymous"></script>
+  </head>
+  <body>
+	<?php
+	include "header.php";
+	?>
+<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<h1> </h1>
+		</div>
+	</div>
+		<div class="col-sm-12 col-md-6 col-lg-6">
+			<h3>Contacto</h3><hr/>
+			    <table id="contacto">
+			        <tr height="50">
+			            <th width="35"><i class="fas fa-phone-alt fa-sm" aria-hidden="true"></i></th>
+			            <td><a id="tres" href="tel:+34912320320">912 320 320</a></td>
+			        </tr>
+			        <tr height="50">
+			            <th><i class="fas fa-at fa-sm" aria-hidden="true"></i></th>
+			            <td><a id="tres" href="mailto:gruposcercaniasmadrid@renfe.es">gruposcercaniasmadrid@renfe.es</a></td>
+			        </tr>
+			        <tr height="50">
+			            <th><i class="fas fa-map-marker fa-sm" aria-hidden="true"></i></th>
+			            <td><a id="tres" href="https://goo.gl/maps/NdhXcn7QZZigHdTSA">Atocha Renfe</a></td>
+			        </tr></table><br>
+			        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3038.1020157160933!2d-3.6918534851700797!3d40.406590764180976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd4226244847dbdb%3A0x4061e1b42de1195f!2sAtocha%20Cercan%C3%ADas!5e0!3m2!1ses!2ses!4v1622849238287!5m2!1ses!2ses" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+		</div>
+		
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+	</body>
+</html>
